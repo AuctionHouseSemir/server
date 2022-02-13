b49b75626f9ee7b96aaf6be8894865499fa67fa6
Semir Mahovkic <semirmahovkic@192.168.1.18>
2022-02-14 00:58:39 +0100

    initial setup

--- src/main/java/com/house/auction/server/commands/CommandFactory.java
+++ src/main/java/com/house/auction/server/commands/CommandFactory.java
@@ -1,22 +1,32 @@
 package com.house.auction.server.commands;
 
-import com.google.rpc.context.AttributeContext;
 import com.house.auction.server.auth.AuthToken;
 import com.house.auction.server.commands.auction.*;
 import com.house.auction.server.commands.auth.LoginUserCommand;
 import com.house.auction.server.commands.auth.LogoutUserCommand;
 import com.house.auction.server.commands.auth.RegisterUserCommand;
 import com.house.auction.server.commands.invalid.InvalidCommand;
+import com.house.auction.server.commands.invalid.InvalidCommandParameters;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 public class CommandFactory {
 
+    private static final Logger logger = LoggerFactory.getLogger(CommandFactory.class);
+
     public static Command createInvalidCommand() {
         return new InvalidCommand();
     }
 
+    public static Command createInvalidCommandParameters() {
+        return new InvalidCommandParameters();
+    }
+
     public static Command createRegisterUserCommand(List<String> params) {
+        if (params.size() < 2) return createInvalidCommandParameters();
+
         String username = params.get(0);
         String password = params.get(1);
 
@@ -24,6 +34,8 @@ public class CommandFactory {
     }
 
     public static Command createLoginUserCommand(List<String> params) {
+        if (params.size() < 2) return createInvalidCommandParameters();
+
         String username = params.get(0);
         String password = params.get(1);
 
@@ -35,21 +47,31 @@ public class CommandFactory {
     }
 
     public static Command createCreateAuctionCommand(AuthToken authToken, List<String> params) {
-        String auctionName = params.get(0);
-        double initialPrice = Double.parseDouble(params.get(1));
-        double minPrice = Double.parseDouble(params.get(2));
-        int duration = Integer.parseInt(params.get(3));
-        int userId = authToken.getUserId();
+        try {
+            String auctionName = params.get(0);
+            double initialPrice = Double.parseDouble(params.get(1));
+            double minPrice = Double.parseDouble(params.get(2));
+            int duration = Integer.parseInt(params.get(3));
+            int userId = authToken.getUserId();
 
-        return new CreateAuctionCommand(auctionName, initialPrice, minPrice, duration, userId);
+            return new CreateAuctionCommand(auctionName, initialPrice, minPrice, duration, userId);
+        } catch (Exception e) {
+            logger.error(String.format("failed to create CreateAuctionCommand: %s", e.getMessage()));
+            return createInvalidCommandParameters();
+        }
     }
 
     public static Command createBidCommand(AuthToken authToken, List<String> params) {
-        int auctionId = Integer.parseInt(params.get(0));
-        int bidValue = Integer.parseInt(params.get(1));
-        int userId = authToken.getUserId();
+        try {
+            int auctionId = Integer.parseInt(params.get(0));
+            int bidValue = Integer.parseInt(params.get(1));
+            int userId = authToken.getUserId();
 
-        return new BidCommand(auctionId, bidValue, userId);
+            return new BidCommand(auctionId, bidValue, userId);
+        } catch (Exception e) {
+            logger.error(String.format("failed to create BidCommand: %s", e.getMessage()));
+            return createInvalidCommandParameters();
+        }
     }
 
     public static Command createListActiveAuctionsCommand(AuthToken authToken) {
---
+++ src/main/java/com/house/auction/server/commands/invalid/InvalidCommandParameters.java
@@ -0,0 +1,16 @@
+package com.house.auction.server.commands.invalid;
+
+import com.house.auction.server.commands.Command;
+import com.house.auction.server.commands.ResponseDto;
+import com.house.auction.server.commands.ResponseStatus;
+
+public class InvalidCommandParameters implements Command {
+    @Override
+    public ResponseDto execute() {
+        ResponseDto responseDto = new ResponseDto();
+        responseDto.setStatus(ResponseStatus.Error);
+        responseDto.setContent("invalid command parameters");
+
+        return responseDto;
+    }
+}
