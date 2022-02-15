5cb28ac8a2527a66d9d9216e5851d62c7907072d
Semir Mahovkic <semirmahovkic@192.168.1.3>
2022-02-15 23:48:17 +0100

    server

--- src/main/java/com/house/auction/server/adapters/CacheStorageAdapter.java
+++ src/main/java/com/house/auction/server/adapters/CacheStorageAdapter.java
@@ -4,20 +4,35 @@ import com.house.auction.server.cache.CacheItem;
 import com.house.auction.server.cache.CacheStorage;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 @Service
 public class CacheStorageAdapter implements CacheStorage {
+    private final ConcurrentHashMap<String, String> storage;
+
+    public CacheStorageAdapter() {
+        this.storage = new ConcurrentHashMap<>();
+    }
+
     @Override
     public void store(String key, String value) {
-
+        this.storage.put(key, value);
     }
 
     @Override
     public CacheItem get(String key) {
-        return null;
+        String value = storage.get(key);
+        if (value == null) return null;
+
+        CacheItem cacheItem = new CacheItem();
+        cacheItem.setKey(key);
+        cacheItem.setValue(value);
+
+        return cacheItem;
     }
 
     @Override
     public void destroy(String key) {
-
+        storage.remove(key);
     }
 }
---
+++ src/test/java/com/house/auction/server/adapters/CacheStorageAdapterTests.java
@@ -0,0 +1,43 @@
+package com.house.auction.server.adapters;
+
+import com.house.auction.server.cache.CacheItem;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CacheStorageAdapterTests {
+    private CacheStorageAdapter cacheStorageAdapter;
+
+    @Before
+    public void setup() {
+        cacheStorageAdapter = new CacheStorageAdapter();
+    }
+
+    @Test
+    public void storeAndGet() {
+        cacheStorageAdapter.store("item_key", "item_value");
+
+        CacheItem cacheItem = cacheStorageAdapter.get("item_key");
+        Assert.assertNotNull(cacheItem);
+        Assert.assertEquals("item_key", cacheItem.getKey());
+        Assert.assertEquals("item_value", cacheItem.getValue());
+    }
+
+    @Test
+    public void destroy() {
+        cacheStorageAdapter.store("item_key", "item_value");
+
+        cacheStorageAdapter.destroy("item_key");
+
+        CacheItem cacheItem = cacheStorageAdapter.get("item_key");
+        Assert.assertNull(cacheItem);
+    }
+
+    @Test
+    public void get_KeyNoExists_ReturnsNull() {
+        CacheItem cacheItem = cacheStorageAdapter.get("no_key");
+        Assert.assertNull(cacheItem);
+    }
+}
